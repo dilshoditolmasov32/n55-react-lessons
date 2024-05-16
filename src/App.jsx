68fea5e49027dfc5998e69e60bf5df74d7b69ea2
Dilshodbek20040104 <dilshoditolmasov32@gmail.com>
2024-05-16 09:13:28 +0500

    react-homework

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+// import { NavLink } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <div className="navbar">
+        <li>Works</li>
+        <li>Blog</li>
+        <li>Contact</li>
+      </div>
+
+      <div className="hero">
+        <h1>Hi, I am John, Creative Technologist</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ src/navbar/sd.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const sd = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default sd
